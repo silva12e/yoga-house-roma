950dbd9e16f644a1d13b62bdb3d1b9d302f55b86
edgard silva <edgard.ernesto.silva@gmail.com>
2019-03-03 16:44:25 -0800

    seconds trial

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/index.html'));
+app.use(express.static(__dirname + '/dist/yoga-house-landing'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+res.sendFile(path.join(__dirname+'/dist/yoga-house-landing/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
