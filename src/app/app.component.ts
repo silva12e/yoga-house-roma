bf7f9f5fa312097d1f26e3d2dc6642dad67bc8b3
edgard silva <edgard.ernesto.silva@gmail.com>
2019-03-03 17:53:49 -0800

    remove unused service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from "@angular/core";
-import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +10,6 @@ export class AppComponent {
 
 
   constructor(
-    db: AngularFirestore
   ) {}
 
 }
